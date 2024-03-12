2ba424be905661373c3a7cfbac4420e42f989d8c
EA <eden.agbo@outlook.fr>
2024-03-12 10:01:51 +0100

    Revert "TEST COMMIT"
    This reverts commit c8c3b2e1f4feaa02ab5f3f303645b9f2c79beffd.

--- image_alt_scrapper.py
+++ image_alt_scrapper.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 # Function to check if an image has an "alt" attribute
 def has_alt_attribute(img):
     return "alt" in img.attrs
-# Get the URL from the user TEST MODIF
+# Get the URL from the user
 url = input("Enter the URL of the page to check: ")
 # Make a request to the URL and get the HTML content
 response = requests.get(url)
