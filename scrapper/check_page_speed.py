2dcafc2f33a9491a62e5057851759896bb6889ea
EA <eden.agbo@outlook.fr>
2024-03-12 11:23:33 +0100

    Encapsulation du traitement Check Page Speed dans une classe etnedant Thread.

--- scrapper/check_page_speed.py
+++ scrapper/check_page_speed.py
@@ -3,22 +3,16 @@ import time
 import threading
 
 
-def check_page_speed():
-    # Prompt user to enter the URL to check
-    url = input("Enter the URL to check page speed: ")
-    # Send a GET request to the given URL and record the start time
-    start_time = time.time()
-    response = requests.get(url)
-    # Record the end time and calculate the page load time
-    end_time = time.time()
-    page_load_time = end_time - start_time
-    # Print the page load time in seconds
-    print(f"Page load time: {page_load_time:.2f} seconds")
-    print("Execution dans un thread")
-
-    # Création d'un thread
-    my_thread = threading.Thread(target=check_page_speed)
-    my_thread.start()
-
-
-check_page_speed()
+class CheckPageSpeed(threading.Thread):
+    def run(self):
+        # Prompt user to enter the URL to check
+        url = input("Enter the URL to check page speed: ")
+        # Send a GET request to the given URL and record the start time
+        start_time = time.time()
+        response = requests.get(url)
+        # Record the end time and calculate the page load time
+        end_time = time.time()
+        page_load_time = end_time - start_time
+        # Print the page load time in seconds
+        print(f"Page load time: {page_load_time:.2f} seconds")
+        print("Execution dans un thread")
