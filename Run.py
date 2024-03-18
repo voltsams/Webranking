3b2da01f6cb8d7a2f9db282e5aaaee4bd100ab13
FullStackDev97 <emiliosensuavictor@gmail.com>
2024-03-18 14:51:05 +0100

    word frequency v0

--- Run.py
+++ Run.py
@@ -1,4 +1,5 @@
 from scrapper.CheckPageSpeed import CheckPageSpeed
+from scrapper.WordFrequency import WordFrequency
 import GlobalsModule
 
 GlobalsModule.Globals.prompt_user_to_enter_url(cls=GlobalsModule)
---
+++ entities/Word.py
@@ -0,0 +1,14 @@
+
+class Word(object):
+
+
+    def __init__(self):
+        self.name =""
+        self.frequency =0
+        self.poucentage = 0.00
+
+    def __init__(self, name, frequency, poucentage):
+        self.name = name
+        self.frequency = frequency
+        self.poucentage = poucentage
+
