a24e027c464aaac3a3e7b2ad0e42bca291813328
Sami D <98024370+voltsams@users.noreply.github.com>
2024-03-18 15:10:19 +0100

    Revert "word frequency v0"

--- Run.py
+++ Run.py
@@ -1,5 +1,4 @@
 from scrapper.CheckPageSpeed import CheckPageSpeed
-from scrapper.WordFrequency import WordFrequency
 import GlobalsModule
 
 GlobalsModule.Globals.prompt_user_to_enter_url(cls=GlobalsModule)
